aa44ceda1dcece640718e168eba44962c2a23e12
SSB4455 <741086@gmail.com>
2021-04-29 00:41:13 +0800

    增加保存截图延时 调整Android图库更新检测

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,8 +1,15 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections;
+using System.IO;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+#if UNITY_IOS
+    [System.Runtime.InteropServices.DllImport("__Internal")]
+    private static extern void _SaveToGallery(string filePath);
+#endif
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,51 +24,70 @@ public class NewBehaviourScript : MonoBehaviour
 
     public void SaveImageToGallery()
     {
-        string filename = "Cooking_" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+        string filename = "Screenshot_" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
         string filePath = Application.persistentDataPath + Path.DirectorySeparatorChar + filename;
         Debug.Log("SaveFoodImage " + filePath);
         ScreenCapture.CaptureScreenshot(filename);
+
 #if UNITY_ANDROID
         if (Application.platform == RuntimePlatform.Android)
         {
-            string savePath = "/mnt/sdcard/DCIM/Camera/" + filename;
-            
-            {
-                if (!Directory.Exists(Path.GetDirectoryName(savePath)))
+            WaitingToSaveGallery(0.1f, () => {
+                string savePath = "/mnt/sdcard/DCIM/Camera/" + filename;
                 {
-                    Directory.CreateDirectory(Path.GetDirectoryName(savePath));
-                }
-                //保存文件
-                File.WriteAllBytes(savePath, File.ReadAllBytes(filePath));
+                    if (!Directory.Exists(Path.GetDirectoryName(savePath)))
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(savePath));
+                    }
+                    //保存文件
+                    File.WriteAllBytes(savePath, File.ReadAllBytes(filePath));
 
-                Debug.Log("SaveFoodImage 1" + filePath);
-                //sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(new File("your path"))););
-                AndroidJavaObject currentActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
-                currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
-                {
-                    Debug.Log("SaveFoodImage 2" + filePath);
-                    //Intent scanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                    AndroidJavaObject scanIntent = new AndroidJavaObject("android.content.Intent", new AndroidJavaClass("android.content.Intent").GetStatic<string>("ACTION_MEDIA_SCANNER_SCAN_FILE"));
-                    Debug.Log("SaveFoodImage 3" + filePath);
-                    //scanIntent.setData(Uri.fromFile(new File(filePath)));
-                    scanIntent.Call("setData", Path.GetDirectoryName(savePath));
-                    Debug.Log("SaveFoodImage 4" + filePath);
-                    //getActivity().sendBroadcast(scanIntent);
-                    currentActivity.Call<AndroidJavaObject>("getActivity").Call("sendBroadcast", scanIntent);
-                    Debug.Log("SaveFoodImage 5" + filePath);
-                }));
-            }
+                    Debug.Log("SaveFoodImage 1" + filePath);
+                    //sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(new File("your path"))););
+                    AndroidJavaObject currentActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+                    currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+                    {
+                        Debug.Log("SaveFoodImage 2" + filePath);
+                        //Intent scanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                        AndroidJavaObject scanIntent = new AndroidJavaObject("android.content.Intent", "android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+                        Debug.Log("SaveFoodImage 3" + filePath);
+                        //scanIntent.setData(Uri.fromFile(new File(filePath)));
+                        scanIntent.Call("setData", new AndroidJavaClass("android.net.Uri").CallStatic<AndroidJavaObject>("parse", savePath));
+                        Debug.Log("SaveFoodImage 4" + filePath);
+                        //getActivity().sendBroadcast(scanIntent);
+                        currentActivity.Call<AndroidJavaObject>("getActivity").Call("sendBroadcast", scanIntent);
+                        Debug.Log("SaveFoodImage 5" + filePath);
+                    }));
+                }
+            });
         }
 #elif UNITY_IOS
         if (Application.platform == RuntimePlatform.IPhonePlayer)
         {
-            _SaveToGallery(filePath);
+            WaitingToSaveGallery(0.1f, () => { _SaveToGallery(filePath); });
         }
 #endif
     }
 
-#if UNITY_IOS
-    [System.Runtime.InteropServices.DllImport("__Internal")]
-    private static extern void _SaveToGallery(string filePath);
-#endif
+    IEnumerator WaitingToSaveGallery(float time, Action callback)
+    {
+        yield return new WaitForSeconds(time);
+        callback?.Invoke();
+    }
+
+    void AndroidToast(string text)
+    {
+        AndroidJavaObject currentActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+        currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+        {
+            toastClass.CallStatic<AndroidJavaObject>("makeText", currentActivity, text, toastClass.GetStatic<int>("LENGTH_SHORT")).Call("show");
+        }));
+
+        /*
+        // 匿名方法中第二个参数是安卓上下文对象，除了用currentActivity，还可用安卓中的GetApplicationContext()获得上下文。
+        AndroidJavaObject context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+        */
+    }
+    
 }
